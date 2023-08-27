dce63053a94ae9bd5083ad8e288ae85c79deb2d5
lukasjoho <social@lukashoppe.com>
2023-08-27 23:54:41 +0200

    chore: remove welcome routing

--- app/(authenticated)/space/page.tsx
+++ app/(authenticated)/space/page.tsx
@@ -6,12 +6,9 @@ import { authOptions } from "@/lib/auth"
 
 const SpacePage = async () => {
   const session = await getServerSession(authOptions)
-  if (!session) {
-    redirect("/login")
-  }
-  if (!session?.user?.hasCompletedSignUp) {
-    redirect("/welcome")
-  }
+  // if (!session?.user?.hasCompletedSignUp) {
+  //   redirect("/welcome")
+  // }
   if (!session?.user?.currentSpace?.slug) {
     redirect("/spaces")
   }
