96bed3363dd274a75cb7e6046d995ed4f49564e8
lukasjoho <social@lukashoppe.com>
2023-08-29 05:03:08 +0200

    build: modify report

--- app/(authenticated)/space/[space]/dashboard/components/FeaturedHeader.tsx
+++ app/(authenticated)/space/[space]/dashboard/components/FeaturedHeader.tsx
@@ -2,6 +2,7 @@ import React from "react"
 
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import Text from "@/components/uplift/text"
+import { getExperiments } from "@/app/actions"
 
 const FeaturedHeader = () => {
   return (
@@ -24,13 +25,7 @@ const FeaturedHeader = () => {
 export default FeaturedHeader
 
 const Report = async () => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_HOST_URL}/api/experiments`,
-    {
-      cache: "no-store",
-    }
-  )
-  const experiments = await res.json()
+  const experiments = await getExperiments()
   let stagesCount: any = {
     active: 0,
     completed: 0,
@@ -56,14 +51,15 @@ const Report = async () => {
 
   return (
     <div className="flex gap-8">
-      <Text className="text-2xl font-bold text-green-500 whitespace-nowrap">
+      <Text className="text-xl font-bold text-neutral-500 whitespace-nowrap">
+        {stagesCount.completed} Completed
+      </Text>
+      <Text className="text-xl font-bold text-green-500 whitespace-nowrap">
         {stagesCount.active} Active
       </Text>
-      <Text className="text-2xl font-bold text-blue-500 whitespace-nowrap">
+
+      <Text className="text-xl font-bold text-purple-500 whitespace-nowrap">
         {stagesCount.upcoming} Upcoming
-      </Text>
-      <Text className="text-2xl font-bold text-purple-500 whitespace-nowrap">
-        {stagesCount.completed} Completed
       </Text>
     </div>
   )
