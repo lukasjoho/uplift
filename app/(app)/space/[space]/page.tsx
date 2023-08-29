b6705bed480c38fb80dcea18fa079137c60b0751
lukasjoho <social@lukashoppe.com>
2023-08-29 20:57:42 +0200

    build: enable space redirects

--- app/(app)/space/[space]/page.tsx
+++ app/(app)/space/[space]/page.tsx
@@ -1,16 +1,9 @@
-import Link from "next/link"
 import { redirect } from "next/navigation"
-import { getServerSession } from "next-auth"
-
-import { authOptions } from "@/lib/auth"
-import { prisma } from "@/lib/prisma"
-import { Button } from "@/components/ui/button"
 
 export default async function SpacePage({
   params,
 }: {
   params: { space: string }
 }) {
-  const session = await getServerSession(authOptions)
-  return false
+  redirect(`/space/${params.space}/dashboard`)
 }
--- app/(app)/space/page.tsx
+++ app/(app)/space/page.tsx
@@ -6,14 +6,8 @@ import { authOptions } from "@/lib/auth"
 
 const SpacePage = async () => {
   const session = await getServerSession(authOptions)
-  // if (!session?.user?.hasCompletedSignUp) {
-  //   redirect("/welcome")
-  // }
-  // if (!session?.user?.currentSpace?.slug) {
-  //   redirect("/spaces")
-  // }
-  // redirect(`/space/${session?.user?.currentSpace?.slug}/dashboard`)
-  return <div>space page</div>
+  const spaceSlug = session?.user?.currentSpace?.slug || "uplift"
+  redirect(`/space/${spaceSlug}/dashboard`)
 }
 
 export default SpacePage
