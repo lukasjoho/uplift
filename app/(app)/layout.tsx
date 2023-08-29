b9c5b556bcbef6b99705cd4760e6e09deab3b765
lukasjoho <social@lukashoppe.com>
2023-08-29 17:26:39 +0200

    fix: refactor app layout component

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,23 +1,10 @@
-import { redirect } from "next/navigation"
-import { getServerSession } from "next-auth"
-
-import { authOptions } from "@/lib/auth"
-
 interface AppLayoutProps {
   children: React.ReactNode
   req: any
 }
 
-export default async function AppLayout({ children, req }: AppLayoutProps) {
-  // const headersList = headers()
-  // const session = await getServerSession(authOptions)
-  // if (!session) {
-  //   redirect("/login")
-  // }
-
-  return (
-    <>
-      <div className="flex flex-col grow">{children}</div>
-    </>
-  )
+const AppLayout = ({ children, req }: AppLayoutProps) => {
+  return <div className="flex flex-col grow">{children}</div>
 }
+
+export default AppLayout
