d1933737d106468fb897cac80912d8907868c343
lukasjoho <social@lukashoppe.com>
2023-08-29 12:49:05 +0200

    build: add withauth wrapper to secure app

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,50 @@
+import { redirect } from "next/navigation"
 import { NextRequest, NextResponse } from "next/server"
+import { withAuth } from "next-auth/middleware"
 import { v4 as uuidv4 } from "uuid"
 
-export function middleware(req: NextRequest) {
-  if (!req.cookies.has("anonymousId")) {
+export default withAuth(
+  function middleware(req) {
     const response = NextResponse.next()
-    const uniqueId = uuidv4()
-    response.cookies.set("anonymousId", uniqueId)
+    if (!req.cookies.has("anonymousId")) {
+      const uniqueId = uuidv4()
+      response.cookies.set("anonymousId", uniqueId)
+    }
     return response
+  },
+  {
+    callbacks: {
+      authorized: ({ req, token }) => {
+        console.log("token: ", token)
+        console.log("pathname: ", req.nextUrl.pathname)
+        if (req.nextUrl.pathname.startsWith("/space") && token === null) {
+          return false
+        }
+        return true
+      },
+    },
   }
-  return NextResponse.next()
-}
+)
+
+// export default withAuth(
+//   // function middleware(req: NextRequest) {
+//   //   const response = NextResponse.next()
+//   //   if (!req.cookies.has("anonymousId")) {
+//   //     const uniqueId = uuidv4()
+//   //     response.cookies.set("anonymousId", uniqueId)
+//   //   }
+//   //   return response
+//   // },
+//   {
+//     callbacks: {
+//       authorized({ req, token }) {
+//         if (req.nextUrl.pathname.startsWith("/space")) {
+//           console.log("pathname: ", req.nextUrl.pathname)
+//           console.log("token: ", token)
+//           return NextResponse.redirect("http://localhost:3000/login")
+//         }
+//         return true
+//       },
+//     },
+//   }
+// )
